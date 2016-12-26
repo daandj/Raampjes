82d7da43f03a950fbafdf6a1d13953a45ad7d3e1
Daan de Jong <daanvulkaan@gmail.com>
2016-12-26 13:40:33 +0100

    Fix assembly wrappers for the interrupt handlers.

--- kernel/interrupts_handlers.asm
+++ kernel/interrupts_handlers.asm
@@ -1,50 +1,54 @@
 extern main_intr_handler
 
-%macro handler 1
+%macro handler 2
 global intr%1
 intr%1:
-	xchg bx, bx
+	%if %2 != 1
+		push dword 0
+	%endif
 	pusha
 	cld
-	push byte %1
+	push dword %1
 	call main_intr_handler
+	add sp, 4
 	popa
-	iret
+	add sp, 4
+	iret 
 %endmacro
 
-handler 0
-handler 1
-handler 2
-handler 3
-handler 4
-handler 5
-handler 6
-handler 7
-handler 8
-handler 9
-handler 10
-handler 11
-handler 12
-handler 13
-handler 14
-handler 16
-handler 17
-handler 18
-handler 19
-handler 20
-handler 32
-handler 33
-handler 34
-handler 35
-handler 36
-handler 37
-handler 38
-handler 39
-handler 40
-handler 41
-handler 42
-handler 43
-handler 44
-handler 45
-handler 46
-handler 47
+handler 0, 0
+handler 1, 0
+handler 2, 0
+handler 3, 0
+handler 4, 0
+handler 5, 0
+handler 6, 0
+handler 7, 0
+handler 8, 1
+handler 9, 0
+handler 10, 1
+handler 11, 1
+handler 12, 1
+handler 13, 1
+handler 14, 1
+handler 16, 0
+handler 17, 1
+handler 18, 0
+handler 19, 0
+handler 20, 0
+handler 32, 0
+handler 33, 0
+handler 34, 0
+handler 35, 0
+handler 36, 0
+handler 37, 0
+handler 38, 0
+handler 39, 0
+handler 40, 0
+handler 41, 0
+handler 42, 0
+handler 43, 0
+handler 44, 0
+handler 45, 0
+handler 46, 0
+handler 47, 0
