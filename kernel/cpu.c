c05691fbcb8902ba374a9152ff84434749d6c34f
Daan de Jong <daanvulkaan@gmail.com>
2017-01-25 20:48:32 +0100

    Add function for reading the control registers.

--- kernel/cpu.c
+++ kernel/cpu.c
@@ -2,11 +2,17 @@
 #include <cpu.h>
 #include <interrupts.h>
 
-void outb(uint16_t port, uint8_t data) {
+#define READ_CR(x) inline unsigned long read_cr ## x(void) { \
+	unsigned long val; \
+	asm volatile ( "mov %%cr" # x ", %0" : "=r"(val) ); \
+	return val; \
+}
+
+inline void outb(uint16_t port, uint8_t data) {
 	asm ("outb %0, %1" : : "a"(data), "Nd"(port));
 }
 
-uint8_t inb(uint16_t port) {
+inline uint8_t inb(uint16_t port) {
 	uint8_t _return;
 	asm ("inb %1, %0" : "=a"(_return) : "Nd"(port));
 	return _return;
@@ -17,6 +23,12 @@ void kernel_halt() {
 	asm ("hlt");
 }
 
-void vm_page_inval(uintptr_t address) {
-	asm ("invlpg (%0)" :: "a" (address));
+inline void vm_page_inval(uintptr_t address) {
+	asm volatile ("invlpg (%0)" :: "a" (address));
 }
+
+READ_CR(0)
+READ_CR(1)
+READ_CR(2)
+READ_CR(3)
+READ_CR(4)
--- kernel/include/cpu.h
+++ kernel/include/cpu.h
@@ -4,3 +4,7 @@ void outb(uint16_t port, uint8_t data);
 uint8_t inb(uint16_t port);
 void kernel_halt();
 void vm_page_inval(uintptr_t address);
+unsigned long read_cr0(void);
+unsigned long read_cr2(void);
+unsigned long read_cr3(void);
+unsigned long read_cr4(void);
